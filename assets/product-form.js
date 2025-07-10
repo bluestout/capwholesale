8ac120dd141fc291016ea7128dc5fdeb60edf20a
kdu1203 <albersdam2000@gmail.com>
2025-07-10 01:30:25 -0700

    Product Page

--- assets/product-form.js
+++ assets/product-form.js
@@ -18,6 +18,7 @@ if (!customElements.get('product-form')) {
       }
 
       onSubmitHandler(evt) {
+        console.log(evt, "⚡⚡⚡⚡");
         evt.preventDefault();
         if (this.submitButton.getAttribute('aria-disabled') === 'true') return;
 
